2385b8c90c30c31778e6511650b9c348a7f70fdd
Qingteng0407 <kbdw2021@leeds.ac.uk>
2026-02-21 03:59:53 +0000

    2.59

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: QingtengZhang
+Student ID: 201821913
 */
 
 #include <stdio.h>
@@ -10,10 +10,24 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
-
+    int n = argc - 1;
+    char buffer[100]; // make sure this is big enough to hold answer
     // process the command-line data using appropriate string functions
+    if (n < 1||n > 10) {
+        return 1;
 
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+        }
 
+    for (int i = 1;i <= n; i++) {
+        int len = strlen(argv[i]);
+
+        printf("%s",argv[i]);
+
+        if (i < n) {
+                printf("-");
+            }
+    }
+    
+    
     return 0;
-}
+    }   
